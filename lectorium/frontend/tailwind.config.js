ffc77edfbf8e0c1f65ba885a1bc6a708d1629a6c
sin_pin <artistsin20@gmail.com>
2023-11-20 19:14:28 +0300

    fix

--- lectorium/frontend/tailwind.config.js
+++ lectorium/frontend/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white': '#FFFFFF',
+        'black': '#000000',
+        'light-gray': '#f4f7fe',
+        'secondary': '#2B3674',
+        'text-prim': '#A3AED0',
+      },
+    },
+    fontFamily: {
+      DMSansRegular: ['DMSans-regular', 'sans-serif'],
+      DMSansMedium: ['DMSans-medium','sans-serif'],
+      DMSansBold: ['DMSans-bold','sans-serif'],
+    },
+
   },
   plugins: [],
 }
