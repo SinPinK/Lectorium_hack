2ffab1cd29d8ce62ef6870b0b60cf71a46b1349b
Nikita <123288888+spyr0xz@users.noreply.github.com>
2023-11-20 16:07:44 +0300

    ts convert

--- lectorium/frontend/src/App.tsx
+++ lectorium/frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Go Hack</h1>
+     
     </div>
   );
 }
