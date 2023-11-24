a0d71761d2e61811abb4f5599450f868279e12f5
Nikita <123288888+spyr0xz@users.noreply.github.com>
2023-11-24 20:53:47 +0300

    add async

--- lectorium/frontend/src/pages/MainPage/MainPage.tsx
+++ lectorium/frontend/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DropCard } from "../../components/UI/DropCard";
 import { TextBlock } from "../../components/TextBlock";
 import List from "../../components/UI/List/List";
 import { IConcept, ITranscribedText } from "../../utils/types";
 import { ConceptItem } from "../../components/UI/Concept/ConceptItem";
+import axios from "axios";
 
 export const MainPage: React.FC = () => {
   const [text, setText] = useState({
@@ -13,57 +14,65 @@ export const MainPage: React.FC = () => {
   const [conc, setConc] = useState([
     {
       id: 0,
-      title: 'Машина',
-      body: 'это что ваще такое'
-  }
-])
+      title: "Машина",
+      body: "это что ваще такое",
+    },
+  ]);
 
-  const [par, setPar] = useState<ITranscribedText[]>([{
-    id: 0,
-    body:{
-      part1:'lol',
-      part2:'lol2',
-      part3:'lol3',
-      part4:'lol4',
-      part5:'lol2',
-      part6:'lol2',
-      part7:'lol2',
-      part8:'lol2',
-      part9:'lol2',
-      part10:'lol2',
-      part11:'lol2'
+  const [content, setContent] = useState<ITranscribedText[]>([]);
+  const [isLoading, setIsLoading] = useState(false)
 
+  // const [par, setPar] = useState<ITranscribedText[]>([{
+  //   id: 0,
+  //   body:{
+  //     part1:'part1',
+  //     part2:'lol2',
+  //     part3:'lol3',
+  //     part4:'lol4',
+  //     part5:'lol2',
+  //     part6:'lol2',
+  //     part7:'lol2',
+  //     part8:'lol2',
+  //     part9:'lol2',
+  //     part10:'lol2',
+  //     part11:'lol2'
+
+  //   }
+  // }])
+
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    try {
+      setIsLoading(true);
+      const response = await axios.get<ITranscribedText[]>(
+        window.location.origin + "/api/test/"
+      );
+      setContent(response.data);
+      console.log(content);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
     }
-  }])
-
-  
-
-  
- 
+  }
 
   return (
-    // <div className=" flex gap-8 flex-col">
-    //   <div className="flex gap-8 flex-row">
-    //   <DropCard />
-    //   <List
-    //     items={}
-    //     renderItem={(item) => <ConceptItem props={item} key={item.id} />}
-    //   />
-    //   </div>
-      
-    //   <TextBlock props={text} />
-    // </div>
-
     <div className="flex flex-col gap-8">
       <div className="flex gap-8 flex-row">
-      <DropCard />
-      <List
-        items={conc}
-        renderItem={(item) => <ConceptItem props={item} key={item.id} />}
-      />
+        <DropCard />
+        <List
+          items={conc}
+          renderItem={(item) => <ConceptItem props={item} key={item.id} />}
+        />
       </div>
-      
-      <List items={par} renderItem={(item) => <TextBlock props={item} key={item.id} /> } />
+
+      <List
+        items={content}
+        renderItem={(item) => <TextBlock props={item} key={item.id} />}
+      />
     </div>
   );
 };
